b24577961b66604ab024f72e8c525c1fa74951f1
Jawid Fakhari <jawid_fakhari@live.it>
2024-04-23 14:40:31 +0200

    lets see

--- src/components/socialIcons/SocialIcons.jsx
+++ src/components/socialIcons/SocialIcons.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Styled from './socialIcons.module.css';
-import { BsGithub, BsLinkedin, } from 'react-icons/bs';
+
 
 
 function SocialIcons() {
@@ -11,8 +11,8 @@ function SocialIcons() {
                     <p><span className={Styled.name}>Jawid</span>Fakhari</p>
                 </div>
                 <div className={Styled.socialIcons}>
-                    <a href="https://github.com/jawid-fakhari" target='_blank' rel="noreferrer"><BsGithub /></a>
-                    <a href="https://www.linkedin.com/in/fakhari-jawid/" target='_blank' rel="noreferrer"><BsLinkedin /></a>
+                    <a href="https://github.com/jawid-fakhari" target='_blank' rel="noreferrer"></a>
+                    <a href="https://www.linkedin.com/in/fakhari-jawid/" target='_blank' rel="noreferrer"></a>
                 </div>
             </div>
         </>
